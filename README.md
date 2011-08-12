33c00b708bd10b6cbd6cecd30ccffb747c1f5c68
icebreaker <theicebreaker007@gmail.com>
2011-08-12 14:43:56 +0300

    initial import

---
+++ README.md
@@ -0,0 +1,7 @@
+Gwen
+====
+This is a fork of [Gwen](http://code.google.com/p/gwen/) a *GUI system aimed at games* by [Facepunch Studios](http://www.facepunchstudios.com/).
+
+License
+-------
+Gwen is provided as-is under the **MIT** license. See  *Gwen.h* for more information.
---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,98 @@
+﻿/*
+	GWEN
+	Copyright (c) 2010 Facepunch Studios
+	See license in Gwen.h
+*/
+
+#include "Gwen/UnitTest/UnitTest.h"
+#include "Gwen/Controls/DockedTabControl.h"
+#include "Gwen/Platform.h"
+
+using namespace Gwen;
+
+#define ADD_UNIT_TEST( name, parent )\
+	GUnit* RegisterUnitTest_##name( Gwen::Controls::Base* tab );\
+	{\
+		GUnit* test = RegisterUnitTest_##name( parent );\
+		test->SetUnitTest( this );\
+		parent->GetTabControl()->AddPage( #name, test );\
+	}\
+
+Gwen::Controls::TabButton* pButton = NULL;
+
+GWEN_CONTROL_CONSTRUCTOR( UnitTest )
+{
+	Dock( Pos::Fill );
+
+	m_TextOutput = new Controls::ListBox( GetRight() );
+	pButton = GetRight()->GetTabControl()->AddPage( "Output", m_TextOutput );
+	GetRight()->SetWidth( 250 );
+
+	m_StatusBar = new Controls::StatusBar( this );
+	m_StatusBar->Dock( Pos::Bottom );
+
+	ADD_UNIT_TEST( TreeControl, GetBottom() );
+	ADD_UNIT_TEST( Button, GetBottom() );
+	ADD_UNIT_TEST( Label, GetBottom() );
+	ADD_UNIT_TEST( Properties, GetBottom() );
+	ADD_UNIT_TEST( TabControl, GetBottom() );
+	ADD_UNIT_TEST( ScrollControl, GetBottom() );	
+	ADD_UNIT_TEST( ComboBox, GetBottom() );
+	ADD_UNIT_TEST( TextBox, GetBottom() );
+	ADD_UNIT_TEST( ListBox, GetBottom() );
+	ADD_UNIT_TEST( Slider, GetBottom() );
+	ADD_UNIT_TEST( ProgressBar, GetBottom() );
+	
+	ADD_UNIT_TEST( ImagePanel, GetBottom()->GetTop() );
+	ADD_UNIT_TEST( Numeric, GetBottom()->GetTop() );
+	ADD_UNIT_TEST( MenuStrip, GetBottom()->GetTop() );
+	ADD_UNIT_TEST( CrossSplitter, GetBottom()->GetTop() );
+	ADD_UNIT_TEST( PanelListPanel, GetBottom()->GetTop() );
+	ADD_UNIT_TEST( GroupBox, GetBottom()->GetTop() );
+	ADD_UNIT_TEST( StatusBar, GetBottom()->GetTop() );
+	ADD_UNIT_TEST( RadioButton, GetBottom()->GetTop() );
+
+	ADD_UNIT_TEST( Checkbox, GetBottom()->GetTop()->GetLeft() );
+
+	GetBottom()->Dock( Pos::Fill );
+	m_StatusBar->SendToBack();
+	
+	PrintText( L"Unit Test Started.\n" );
+
+	m_fLastSecond = Gwen::Platform::GetTimeInSeconds();
+	m_iFrames = 0;
+}
+
+
+void UnitTest::PrintText( const Gwen::UnicodeString& str )
+{
+	m_TextOutput->AddItem( str );
+	m_TextOutput->Scroller()->ScrollToBottom();
+}
+
+void UnitTest::Render( Gwen::Skin::Base* skin )
+{
+	m_iFrames++;
+
+	if ( m_fLastSecond < Gwen::Platform::GetTimeInSeconds() )
+	{
+		m_StatusBar->SetText( Gwen::Utility::Format( L"GWEN Unit Test - %i fps", m_iFrames*2 ) );
+
+		m_fLastSecond = Gwen::Platform::GetTimeInSeconds() + 0.5f;
+		m_iFrames = 0;
+	}
+
+	BaseClass::Render( skin );
+
+}
+
+void GUnit::UnitPrint( const Gwen::UnicodeString& str )
+{
+	m_pUnitTest->PrintText( str );
+}
+
+void GUnit::UnitPrint( const Gwen::String& str )
+{
+	UnitPrint( Utility::StringToUnicode( str ) );
+}
+
